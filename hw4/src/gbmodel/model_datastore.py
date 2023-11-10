28363c78dc1bc92d4b31d32dbc5a01269aeaeafc
addisonwurtz <awurtz@pdx.edu>
2023-11-10 12:09:25 -0800

    update model_datastore.py for quote book

---
+++ hw4/src/gbmodel/model_datastore.py
@@ -0,0 +1,54 @@
+from datetime import date
+from google.cloud import datastore
+from .Model import Model
+
+class model(Model):
+    def __init__(self):
+        self.client = datastore.Client('cloud-wurtz-awurtz')
+
+    def select(self):
+        """
+        Gets all entries from the database
+        Each entry contains: quote, attribution, rating, date_added
+        :return: List of lists containing all rows of database
+        """
+        query = self.client.query(kind = 'GuestEntry')
+        entities = list(map(from_datastore,query.fetch()))
+        return entities
+
+    def insert(self, quote, attribution, rating):
+        """
+        Inserts entry into database
+        :param quote: String
+        :param attribution: String
+        :param rating: float
+        :return: True
+        """
+        key = self.client.key('GuestEntry')
+        entry = datastore.Entity(key)
+        entry.update( {
+            'quote': quote,
+            'attribution': attribution,
+            'rating': rating,
+            'date_added': date.today(),
+            })
+        self.client.put(entry)
+        return True
+
+    def from_datastore(entity):
+        """Translates Datastore results into the format expected by the
+        application.
+
+        Datastore typically returns:
+            [Entity{key: (kind, id), prop: val, ...}]
+
+        This returns:
+            [ quote, attribution, rating, date_added ]
+        where name, email, and message are Python strings
+        and where date is a Python datetime
+        """
+        if not entity:
+            return None
+        if isinstance(entity, list):
+            entity = entity.pop()
+        return [entity['quote'], entity['attribution'], entity['rating'], entity['date_added']]
