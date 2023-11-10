127fa260bc1811e9e1f4ef22372694a63b89b5e2
addisonwurtz <awurtz@pdx.edu>
2023-11-10 11:48:15 -0800

    copied application files to hw4 directory

---
+++ hw4/app/gbmodel/Model.py
@@ -0,0 +1,18 @@
+class Model():
+    def select(self):
+        """
+        Gets all entries from the database
+        :return: Tuple containing all rows of database
+        """
+        pass
+
+    def insert(self, quote, attribution, rating):
+        """
+        Inserts entry into database
+        :param quote: String
+        :param attribution: String
+        :param rating: int
+        :return: none
+        :raises: Database errors on connection and insertion
+        """
+        pass
