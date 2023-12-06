0c4f93e07c7b95fd96066f9aa4c776cf8b49ebbe
Addison <awurtz@pdx.edu>
2023-12-06 03:04:48 +0000

    implemented OAuth

--- final/src/add_contact.py
+++ final/src/add_contact.py
@@ -1,20 +1,44 @@
-from flask import redirect, request, url_for, render_template
+from flask import redirect, request, url_for, render_template, session
+from requests_oauthlib import OAuth2Session
 from flask.views import MethodView
 import gbmodel
+from oauth_config import client_id, authorization_base_url, redirect_callback
 
 class Add(MethodView):
     def get(self):
-        """
-        :return: form template for adding new contacts
-        """
-        return render_template('add_contact.html')
+        # If client has an OAuth2 token, use it to get their information and render
+        #   the signing page with it
+        if 'oauth_token' in session:
+            google = OAuth2Session(client_id, token=session['oauth_token'])
+            userinfo = google.get('https://www.googleapis.com/oauth2/v3/userinfo').json()
+            return render_template('add_contact.html', name=userinfo['name'], email=userinfo['email'], picture=userinfo['picture'])
+        else:
+        # Redirect to the identity provider and ask the identity provider to return the client
+        #   back to /callback route with the code
+            google = OAuth2Session(client_id,
+                    redirect_uri = redirect_callback,
+                    scope = 'https://www.googleapis.com/auth/userinfo.email ' +                   
+                            'https://www.googleapis.com/auth/userinfo.profile'
+            )
+            authorization_url, state = google.authorization_url(authorization_base_url, prompt='login')
+
+            # Identity provider returns URL and random "state" that must be echoed later
+            #   to prevent CSRF.
+            session['oauth_state'] = state
+            return redirect(authorization_url)
+
 
     def post(self):
         """
         Accepts POST requests, and processes the form;
         Redirect to index when completed.
         """
-        model = gbmodel.get_model()
-        contact_history = []
-        model.insert(request.form['first_name'], request.form['last_name'], request.form['orbit'], contact_history)
-        return redirect(url_for('index'))
+        if 'oauth_token' in session:
+        # Insert based on form fields only if an OAuth2 token is present to ensure
+        #   values in all fields exist
+            model = gbmodel.get_model()
+            contact_history = []
+            model.insert(request.form['first_name'], request.form['last_name'], request.form['orbit'], contact_history)
+            return redirect(url_for('index'))
+        else:
+            return redirect(url_for('add_contact'))
--- final/src/app.py
+++ final/src/app.py
@@ -2,16 +2,24 @@
 A simple guestbook flask app.
 """
 import flask
+import os
 from flask.views import MethodView
-
 from add_contact import Add
 from view_contacts import ViewContacts
 from index import Index
+from callback import Callback
+from logout import Logout
 
 app = flask.Flask(__name__)  # our Flask app
 
+app.secret_key = os.urandom(24)
+
 app.add_url_rule('/',
                  view_func=Index.as_view('index'),
+                 methods=["GET"])
+
+app.add_url_rule('/callback',
+                 view_func=Callback.as_view('callback'),
                  methods=["GET"])
 
 app.add_url_rule('/add_contact',
@@ -20,6 +28,10 @@ app.add_url_rule('/add_contact',
 
 app.add_url_rule('/view_contacts',
                  view_func=ViewContacts.as_view('view_contacts'),
+                 methods=["GET"])
+
+app.add_url_rule('/logout',
+                 view_func=Logout.as_view('logout'),
                  methods=["GET"])
 
 if __name__ == '__main__':
