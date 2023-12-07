636c3344be5fa1912200ffa66f6593736e34dfe1
Addison <awurtz@pdx.edu>
2023-12-07 03:53:24 +0000

    only display contacts of authorized user

--- final/src/view_contacts.py
+++ final/src/view_contacts.py
@@ -16,7 +16,11 @@ class ViewContacts(MethodView):
             google = OAuth2Session(client_id, token=session['oauth_token'])
             userinfo = google.get('https://www.googleapis.com/oauth2/v3/userinfo').json()
             model = gbmodel.get_model()
-            contacts = [dict(first_name=row[0], last_name=row[1], orbit=row[2], contact_history=row[3], date_added=row[4] ) for row in model.select()]
+            # make list of dictionaries containing only contacts that belong to authorized user
+            contacts = [dict(user_email=row[0], first_name=row[1], last_name=row[2], orbit=row[3], contact_history=row[4], date_added=row[5]) for row in model.select() if row[0] == userinfo['email']]
+            
+            # TODO compare contacts to active tasks and create new task for each contact that does not have one
+            
             return render_template('view_contacts.html', contacts=contacts, name=userinfo['name'], email=userinfo['email'], picture=userinfo['picture'])
         else:
         # Redirect to the identity provider and ask the identity provider to return the client
