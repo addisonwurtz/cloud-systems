988047be46b0db917ea147fcdb72d19b383fa0fa
Addison <awurtz@pdx.edu>
2023-12-07 02:18:06 +0000

    removed 'Contacts' display from main page

--- final/src/index.py
+++ final/src/index.py
@@ -9,5 +9,4 @@ class Index(MethodView):
         :return: List of dicts of all quote entries in the database
         """
         model = gbmodel.get_model()
-        contacts = [dict(first_name=row[0], last_name=row[1], orbit=row[2], contact_history=row[3], date_added=row[4]) for row in model.select()]
-        return render_template('index.html', contacts=contacts)
+        return render_template('index.html')
